388682bed30c35a89e165651d5f418b69aa012d5
HugoPizzaJetsky <gabrielbernardelli789@gmail.com>
2026-03-05 19:16:16 -0400

    versaoFinal

--- servidor/tcp_servidor.py
+++ servidor/tcp_servidor.py
@@ -1,16 +1,6 @@
 import socket
 import hashlib
 
-def calcula_hash(caminho):
-    hash_sha256 = hashlib.sha256() #cria o objeto sha256
-    with open(caminho, "rb") as f: #abre o arquivo em modo binário
-        while True:
-            pedaco = f.read(4096) #lê 4096 bytes por vez
-            if not pedaco: #se acabou de ler para
-                break
-            hash_sha256.update(pedaco) #adiciona o pedaço e atualiza o hash
-    return hash_sha256.hexdigest() #retorna o hash em texto hexadecimal
-
 IP = '0.0.0.0'
 porta = 5000
 
@@ -48,18 +38,18 @@ while True:
                 raise ConnectionError("Cliente desconectou no arquivo")
             bytes_recebidos += parte
 
-        #salvar o arquivo
-        caminho = f"recebido_{nome}"
-        with open(caminho, "wb") as f:
-            f.write(bytes_recebidos)
-
         #calcular o hash
-        hash_calculado = calcula_hash(caminho)
+        hash_calculado = hashlib.sha256(bytes_recebidos).hexdigest()
 
         #comparar hashes
         if hash_calculado == hash_cliente:
             conexao.sendall("hash certo".encode())
             print("Arquivo íntegro.")
+
+            #salvar o arquivo
+            caminho = f"recebido_{nome}"
+            with open(caminho, "wb") as f:
+                f.write(bytes_recebidos)
         else:
             conexao.sendall("hash errado".encode())
             print("Hash inválido!")
